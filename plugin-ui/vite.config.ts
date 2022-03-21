d1ccb6805488421ed1f86ae986dc401de148dfd6
wanlijun <wanlijun_cq@126.com>
2022-03-21 17:45:17 +0800

    feat:修改构建命令

--- plugin-ui/vite.config.ts
+++ plugin-ui/vite.config.ts
@@ -2,48 +2,56 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import usePluginImport from "vite-plugin-importer";
 import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"),
-    },
-  },
-  root: "./src",
-  base: "/",
-  plugins: [
-    react(),
-    usePluginImport({
-      libraryName: "antd",
-      libraryDirectory: "es",
-      style: true,
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+//console.log(process.env, '=????++++++++++++++++++==')
+export default defineConfig(({ mode }) => {
+  const baseConfig = {
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src"),
       },
     },
-  },
-  build: {
-    outDir: "../dist",
-    // assetsDir: "../assets",
-    rollupOptions: {
-      input: {
-        form: path.resolve(__dirname, "src/formEntry/index.html"),
-        list: path.resolve(__dirname, "src/listEntry/index.html"),
-      },
-      output: {
-        format: "cjs",
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name][extname]",
-        manualChunks: () => {
-          return null;
+    root: "./src",
+    base: "/",
+    plugins: [
+      react(),
+      usePluginImport({
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: true,
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
         },
       },
     },
-  },
-});
+    build: {
+      outDir: "/home/lijunwan/caicai/project/best/react-practice/generate-code-plugin/ui",
+      // assetsDir: "../assets",
+      rollupOptions: {
+        input: {},
+        output: {
+          compact: true,
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+          assetFileNames: "[name][extname]",
+          manualChunks: () => {
+            return null;
+          },
+        },
+      },
+    },
+  }
+  if (mode === 'form') {
+    baseConfig.build.rollupOptions.input = {
+      form: path.resolve(__dirname, "src/formEntry/index.html")
+    }
+  } else if (mode === 'list') {
+    baseConfig.build.rollupOptions.input = {
+      list: path.resolve(__dirname, "src/listEntry/index.html")
+    }
+  }
+  return baseConfig;
+})
